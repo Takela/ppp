b12ec017d117df65508348fa8477ddc1c576ef58
Morgen Morgen <kirill6323236323@mail.ru>
2021-11-27 14:58:46 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-data-table dark
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    item-key="name"
+    class="elevation-1"
+    :footer-props="{
+      showFirstLastPage: true,
+      firstIcon: 'mdi-arrow-collapse-left',
+      lastIcon: 'mdi-arrow-collapse-right',
+      prevIcon: 'mdi-minus',
+      nextIcon: 'mdi-plus'
+    }"
+  ></v-data-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: '№',
+            align: 'align-content',
+            value: 'number',
+          },
+          { text: 'Действие',value: 'name', },
+
+          {
+            text: 'Время',value: 'gregori',
+          }
+        ],
+        desserts: [
+          {
+            number: '1',
+            name: 'проснутся',
+            gregori: '9:00',
+          },
+          {
+            number: '2',
+            name: 'Умыться',
+            gregori: '9:10',
+          },
+          {
+            number: '3',
+            name: 'Покушать или попить чай',
+            gregori: '12:00-12:30',
+          },
+          {
+            number: '4',
+            name: 'школа',
+            gregori: '12:40-19:00',
+          },
+          {
+            number: '5',
+            name: 'Дота?',
+            gregori: '20:00-02:00',
+          },
+          
+        ],
+      }
+    },
+  }
+</script>
+
+
+<style>
+
+</style>
